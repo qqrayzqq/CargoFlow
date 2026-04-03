7e22598e36ead8a365077d0877413aa44dad07fe
asirb <asirbabayanvlad@gmail.com>
2026-04-04 00:31:47 +0200

    init: project setup and DB migrations

---
+++ src/test/java/com/github/qqrayzqq/cargoflow_practice/CargoflowPracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.qqrayzqq.cargoflow_practice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CargoflowPracticeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
