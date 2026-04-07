6a6524030f240e44b15262cbacb3bb4ccaf8cb01
asirb <asirbabayanvlad@gmail.com>
2026-04-08 01:27:55 +0200

    chore: add GitHub Actions CI pipeline with postgres service

--- src/test/java/com/github/qqrayzqq/cargoflow_practice/CargoflowPracticeApplicationTests.java
+++ src/test/java/com/github/qqrayzqq/cargoflow_practice/CargoflowPracticeApplicationTests.java
@@ -1,10 +1,15 @@
 package com.github.qqrayzqq.cargoflow_practice;
 
+import com.github.qqrayzqq.cargoflow_practice.elasticsearch.repository.AddressSearchRepository;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
 
 @SpringBootTest
 class CargoflowPracticeApplicationTests {
+
+    @MockitoBean
+    AddressSearchRepository addressSearchRepository;
 
     @Test
     void contextLoads() {
