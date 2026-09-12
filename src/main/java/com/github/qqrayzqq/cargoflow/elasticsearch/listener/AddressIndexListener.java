9d4882e0abc06f33a1a7f7ccc8fa8f072e498cc2
qqrayzqq <162329881+qqrayzqq@users.noreply.github.com>
2026-09-12 20:57:32 +0200

    fix: don't fail shipment creation when Elasticsearch indexing fails

--- src/main/java/com/github/qqrayzqq/cargoflow/elasticsearch/listener/AddressIndexListener.java
+++ src/main/java/com/github/qqrayzqq/cargoflow/elasticsearch/listener/AddressIndexListener.java
@@ -4,10 +4,13 @@ import com.github.qqrayzqq.cargoflow.elasticsearch.document.AddressDocument;
 import com.github.qqrayzqq.cargoflow.elasticsearch.dto.AddressEventDTO;
 import com.github.qqrayzqq.cargoflow.elasticsearch.repository.AddressSearchRepository;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.dao.DataAccessException;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.event.TransactionPhase;
 import org.springframework.transaction.event.TransactionalEventListener;
 
+@Slf4j
 @Component
 @RequiredArgsConstructor
 public class AddressIndexListener {
@@ -16,6 +19,11 @@ public class AddressIndexListener {
     @TransactionalEventListener(phase = TransactionPhase.AFTER_COMMIT)
     public void createDocument(AddressEventDTO dto){
         AddressDocument newDocument = new AddressDocument(dto.id().toString(), dto.city(), dto.street(), dto.zip(), dto.country(), dto.buildingNumber());
-        addressSearchRepository.save(newDocument);
+        try {
+            addressSearchRepository.save(newDocument);
+        }
+        catch (DataAccessException e){
+            log.error("ES index error {} caused by {}", e.getMessage(), e.getCause(), e);
+        }
     }
 }
